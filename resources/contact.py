7471d46fcdf78c7b0788feea8fff798d17ac5fcd
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 18:18:29 -0600

    Modify Contact and ContactList Resources

--- resources/contact.py
+++ resources/contact.py
@@ -1,17 +1,17 @@
 from flask_restful import Resource
 
 class Contact(Resource):
-    def get(self):
-        return {"content": "GET: contact"}
+    def get(self, email: str):
+        return {"content": f"GET: contact <email={email}>"}
     
-    def post(self):
-        return {"content": "POST: contact"}
+    def post(self, email: str):
+        return {"content": f"POST: contact <email={email}>"}
 
-    def put(self):
-        return {"content": "PUT: contact"}
+    def put(self, email: str):
+        return {"content": f"PUT: contact <email={email}>"}
 
-    def delete(self):
-        return {"content": "DELETE: contact"}
+    def delete(self, email: str):
+        return {"content": f"DELETE: contact <email={email}>"}
 
 class ContactList(Resource):
     def get(self):
