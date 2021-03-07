d4ebd3aa90c4ca0c96ed0342d943b1e6ffb43614
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 21:12:21 -0600

    Modify get method in the Contact Resource

--- resources/contact.py
+++ resources/contact.py
@@ -1,16 +1,21 @@
+from flask import request
 from flask_restful import Resource
 from schemas.contact import ContactSchema
 from models.contact import ContactModel
+from marshmallow import ValidationError
 
 contact_schema = ContactSchema()
 contact_list_schema = ContactSchema(many=True)
 
 class Contact(Resource):
     def get(self, email: str):
-        return {"content": f"GET: contact <email={email}>"}
+        contact = ContactModel.find_by_email(email)
+        if contact:
+            return contact_schema.dump(contact)
+        return {"message": f"ERROR: Couldn't find requested contact <email={email}>"}
 
     def post(self, email: str):
-        return {"content": f"POST: contact <email={email}>"}
+        pass
 
     def put(self, email: str):
         return {"content": f"PUT: contact <email={email}>"}
