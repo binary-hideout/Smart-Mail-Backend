a79d547ca6ab6e59f903008c3757a6797b53f2e0
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 19:07:29 -0600

    Add database

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_restful import Api
+
 from resources.contact import Contact, ContactList
+
+from db import db
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 api.add_resource(Contact, "/contact/<string:email>")
 api.add_resource(ContactList, "/contacts")
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(debug=True)
