fd45581d90f779f046add892ebd7731f93bcde15
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 18:22:54 -0600

    Reformat contact.py

--- resources/contact.py
+++ resources/contact.py
@@ -1,9 +1,10 @@
 from flask_restful import Resource
 
+
 class Contact(Resource):
     def get(self, email: str):
         return {"content": f"GET: contact <email={email}>"}
-    
+
     def post(self, email: str):
         return {"content": f"POST: contact <email={email}>"}
 
@@ -12,6 +13,7 @@ class Contact(Resource):
 
     def delete(self, email: str):
         return {"content": f"DELETE: contact <email={email}>"}
+
 
 class ContactList(Resource):
     def get(self):
