6482af971d540690c3eeb195542d781c26a55c23
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 18:23:11 -0600

    Reformat app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from resources.contact import Contact, ContactList
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Contact, '/contact/<string:email>')
-api.add_resource(ContactList, '/contacts')
+api.add_resource(Contact, "/contact/<string:email>")
+api.add_resource(ContactList, "/contacts")
 
 if __name__ == "__main__":
     app.run(debug=True)
