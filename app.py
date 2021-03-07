daa4550794c0a1b6b82d6a8d8a292276060fc1cd
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-06 18:19:04 -0600

    Add Contact and ContactList endpoints to the API

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+from resources.contact import Contact, ContactList
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Contact, '/contact/<string:email>')
+api.add_resource(ContactList, '/contacts')
+
+if __name__ == "__main__":
+    app.run(debug=True)
