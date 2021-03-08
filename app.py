7168e388f7be61507d08c54785e43bb4ae31d35f
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-07 19:16:09 -0600

    Add SQLAlchemy and Marshmallow app initializers for production deployment

--- app.py
+++ app.py
@@ -14,13 +14,17 @@ app.config.from_object('default_settings')
 app.config.from_envvar('APPLICATION_SETTINGS')
 api = Api(app)
 
+# For production
 @app.before_first_request
 def create_tables():
+    db.init_app(app)
+    ma.init_app(app)
     db.create_all()
 
 api.add_resource(Contact, "/contact/<string:email>")
 api.add_resource(ContactList, "/contacts")
 
+# For local development
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
