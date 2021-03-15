b1517971cc09078c91063c435325f206eef0d9af
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-15 17:24:18 -0600

    Add TagSchema.

---
+++ schemas/tag.py
@@ -0,0 +1,14 @@
+from ma import ma
+from models.tag import TagModel
+from models.case import CaseModel
+from schemas.case import CaseSchema
+
+class TagSchema(ma.SQLAlchemyAutoSchema):
+    cases = ma.Nested(CaseSchema, many=True)
+
+    class Meta:
+        model = TagModel
+        load_only = ("tag",)
+        dump_only = ("id",)
+        include_fk = True
+        load_instance = True
