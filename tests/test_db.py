28af01ae5a8128b3cd0f616834affc40962d58d1
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-19 19:20:44 -0600

    Add SQLAlchemy models tests.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,51 @@
-import os
-import tempfile
+from models.contact import ContactModel
+from models.tag import TagModel
+from models.case import CaseModel
 
-from smartmail import create_app
+def test_contactmodel_save(app):
+    with app.app_context():
+        contact = ContactModel(first_name="John", last_name="Doe", email="john_doe@gmail.com", phone="8117266267")
+        contact.save_to_db()
+        assert ContactModel.find_by_email('john_doe@gmail.com')
 
-def test_config():
-    assert not create_app().testing
-    assert create_app({'TESTING': True}).testing
+def test_tagmodel_save(app):
+    with app.app_context():
+        tag = TagModel(title="To be resolved", color="red")
+        tag.save_to_db()
+        assert TagModel.find_by_title('To be resolved')
 
+def test_casemodel_save(app):
+    with app.app_context():
+        case = CaseModel(title="Car accident", description="car accident that damaged the car", tag_id=1, contact_id=1)
+        case.save_to_db()
+        assert CaseModel.find_by_title('Car accident')
 
-def test_hello(client):
-    response = client.get('/contacts')
-    assert response.data == b'{"content": []}'
+def test_contactmodel_all(app):
+    with app.app_context():
+        assert ContactModel.find_all()
+
+def test_tagmodel_all(app):
+    with app.app_context():
+        assert TagModel.find_all()
+
+def test_casemodel_all(app):
+    with app.app_context():
+        assert CaseModel.find_all()
+
+def test_casemodel_delete(app):
+    with app.app_context():
+        case = CaseModel.find_by_title('Car accident')
+        case.delete_from_db() 
+        assert CaseModel.find_by_title('Car accident') == None
+
+def test_tagmodel_delete(app):
+    with app.app_context():
+        tag = TagModel.find_by_title('To be resolved')
+        tag.delete_from_db() 
+        assert TagModel.find_by_title('To be resolved') == None
+
+def test_contactmodel_delete(app):
+    with app.app_context():
+        contact = ContactModel.find_by_email('john_doe@gmail.com')
+        contact.delete_from_db() 
+        assert ContactModel.find_by_email('john_doe@gmail.com') == None
