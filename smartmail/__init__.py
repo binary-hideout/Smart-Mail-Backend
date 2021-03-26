47e484b955d8dca5e76b8caf5ef8efb1aa3524d6
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-26 14:09:37 -0600

    modify files

--- smartmail/__init__.py
+++ smartmail/__init__.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=os.path.join(app.instance_path, "smartmail.sqlite"),
@@ -22,7 +22,7 @@ def create_app(test_config=None):
     load_dotenv(".env", verbose=True)
     if test_config is None:
         app.config.from_pyfile("default_settings.py", silent=True)
-        app.config.from_envvar("APPLICATION_SETTINGS")
+        # app.config.from_envvar("APPLICATION_SETTINGS")
     else:
         app.config.from_pyfile("testing_settings.py", silent=True)
         app.config.from_envvar("TESTING_SETTINGS")
