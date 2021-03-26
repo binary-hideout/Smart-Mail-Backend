750f6633dea4b04d6f444a28a1569703d12aa817
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-03-26 11:01:02 -0600

    Modify files

--- smartmail/webapp.py
+++ smartmail/webapp.py
@@ -1,1 +1,16 @@
 from . import app
+
+import os
+
+from flask import Flask
+from flask_restful import Api
+
+from resources.contact import Contact, ContactList
+from resources.case import Case, CaseList
+from resources.tag import Tag, TagList
+
+from apps.db import db
+from apps.ma import ma
+
+from dotenv import load_dotenv
+
