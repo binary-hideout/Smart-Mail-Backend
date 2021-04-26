0b65b7212013f92bdc2ebec05f77524b067002ee
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-04-26 15:47:54 -0500

    Add a user model

---
+++ smartmail/models/user.py
@@ -0,0 +1,29 @@
+from typing import List
+from smartmail.apps.db import db
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username: str) -> "UserModel":
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_email(cls, email: str) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def find_by_id(cls, _id: int):
+        return cls.query.filter_by(id=_id).first()
