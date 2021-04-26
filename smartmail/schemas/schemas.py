87494ef6926e70fabe604f1843eedd14fd4662eb
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-04-26 15:47:23 -0500

    Add a user schema

--- smartmail/schemas/schemas.py
+++ smartmail/schemas/schemas.py
@@ -1,7 +1,10 @@
 from smartmail.apps.ma import ma
+from marshmallow import fields
 from smartmail.models.contact import ContactModel
 from smartmail.models.tag import TagModel
 from smartmail.models.case import CaseModel
+from smartmail.models.user import UserModel
+
 
 class CaseSchema(ma.SQLAlchemyAutoSchema):
     contact = ma.Nested(lambda: ContactSchema(only=("id", "email", "phone")))
@@ -12,6 +15,7 @@ class CaseSchema(ma.SQLAlchemyAutoSchema):
         dump_only = ("id", "created")
         include_fk = True
         load_instance = True
+
 
 class ContactSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -32,3 +36,15 @@ class TagSchema(ma.SQLAlchemyAutoSchema):
         dump_only = ("id",)
         include_fk = True
         load_instance = True
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserModel
+        load_only = ("password",)
+        dump_only = ("id",)
+        load_instance = True
+
+    id = fields.Int()
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
