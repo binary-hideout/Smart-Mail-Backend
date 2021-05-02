04f7fc7a8f1a2732dc404de55c3a6b7a69d7aaa4
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-05-02 16:59:06 -0500

    Modify files

--- smartmail/resources/call.py
+++ smartmail/resources/call.py
@@ -9,17 +9,19 @@ from flask_restful import Resource
 from smartmail.schemas.schemas import ContactSchema
 from smartmail.models.contact import ContactModel
 from smartmail.apps.android_manager import phone_call, save_text, play_sound, adb_exec_path, mpg123_exec_path, sounds_dir_path
+from smartmail.forms.call import CallForm
 
 contact_schema = ContactSchema()
 
 class Call(Resource):
     def get(self, email: str):
+        call_form  = CallForm()
         contact = ContactModel.find_by_email(email)
         if contact:
             # return contact_schema.dump(contact), 200
             headers = {"Content-Type": "text-html"}
             return make_response(
-                render_template("call.html", result=contact_schema.dump(contact)),
+                render_template("call.html", result=contact_schema.dump(contact), form=call_form),
                 200,
                 headers,
             )
@@ -28,11 +30,13 @@ class Call(Resource):
         }, 404
 
     def post(self, email: str):
-        call_data = request.form.copy()
-        contact = ContactModel.find_by_email(email)
-        if contact:
-            file_path = save_text(call_data['message'], call_data['language'], 'message.mp3')
-            phone_call(adb_exec_path, "7120018020022742", contact.phone)
-            sleep(45)
-            play_sound(mpg123_exec_path, file_path)
-        return redirect(url_for('caselist'))
+        call_form = CallForm()
+        if call_form.validate_on_submit():
+            call_data = request.form.copy()
+            contact = ContactModel.find_by_email(email)
+            if contact:
+                file_path = save_text(call_data['message'], call_data['language'], 'message.mp3')
+                phone_call(adb_exec_path, "7120018020022742", contact.phone)
+                sleep(45)
+                play_sound(mpg123_exec_path, file_path)
+            return redirect(url_for('caselist'))
